afe9a2f54c8cb84725beb3f90bf6fefe928938e5
SuperPuperMegaUltraGovnovoz <168558432+SuperPuperMegaUltraGovnovoz@users.noreply.github.com>
2026-04-03 22:33:43 +0700

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,82 +4,88 @@ import static com.raylib.Colors.*;
 import static com.raylib.Raylib.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        float screenWidth1 = 1240;
+
+        float screenWidth1 = 1280;
         float screenHeight1 = 720;
         SetConfigFlags(FLAG_WINDOW_RESIZABLE);
         InitWindow((int)screenWidth1, (int)screenHeight1, "Demo");
-
         SetTargetFPS(-1);
-        int countObjects = 100;
-        Rectangle player = new Rectangle().x(400).y(280).width(40).height(40);
-        int cols = 10;
 
-        Atom[] atoms = new Atom[countObjects];
-        for (int i = 0; i < countObjects; i++) {
-            int row = i / cols;
-            int col = i % cols;
-            atoms[i] = new Atom((int) player.x() + col * 45,(int) player.y() + row * 45, 20, VIOLET);
-            atoms[i].velocity.x((float) (Math.random() * 4 - 2));
-            atoms[i].velocity.y((float) (Math.random() * 4 - 2));
-        }
-        float dt = 0.005f;
+        Rectangle world = new Rectangle().x(300).y(300).width(40).height(40);
+        Object object = new Object((int)world.x(), (int)world.y(), 90, 20);
 
         Camera2D camera = new Camera2D();
-        camera.target(new Vector2().x(player.x() + 20).y(player.y() + 20));
+        camera.target(new Vector2().x(world.x() + 20).y(world.y() - 60));
         camera.offset(new Vector2().x(screenWidth1/2).y(screenHeight1/2));
         camera.rotation(0.0f);
         camera.zoom(1.0f);
+        float velosityX = 0.2f;
+        float velosityX1 = 0.2f;
+        float velosityY = 0.2f;
+        float velosityY1 = 0.2f;
 
-        float screenWidth = screenWidth1;
-        float screenHeight = screenHeight1;
-
+        Player player = new Player((int)camera.target().x(), (int)camera.target().y(), 30, 40, false);
 
         while (!WindowShouldClose()) {
-            if (IsWindowResized()) {
-                screenWidth = GetScreenWidth();
-                screenHeight = GetScreenHeight();
-            }
-            if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE)){
-                camera.target(Vector2Add(camera.target(), Vector2Scale(GetMouseDelta(), -1.0f/camera.zoom())));
-            }
-            camera.zoom((camera.zoom()) + (GetMouseWheelMove() * 0.03f));
-            if (camera.zoom() <= 0) {
-                camera.zoom(0.009999f);
+            if(player.onFoor){
+                velosityY1 = 0;
+            }else{velosityY1 = 0.2f;}
+            if(player.CollisionWithUp){
+                velosityY = 0;
+            }else{velosityY = 0.2f;}
+            if(player.CollisionWithLeft){
+                velosityX = 0;
+            }else{velosityX = 0.2f;}
+            if(player.CollisionWithRight){
+                velosityX1 = 0;
+            }else{velosityX1 = 0.2f;}
+
+            if(player.onFoor) {
+                if (IsKeyDown(KEY_W)) {
+                    camera.target().y(camera.target().y() + (-velosityY) * GetFrameTime() * 1000);
+                }
             }
 
-            Atom.SimulateAtoms(atoms, dt, screenWidth, screenHeight, countObjects);
+            if (IsKeyDown(KEY_S)) {
+                camera.target().y(camera.target().y() + velosityY1 * GetFrameTime() * 1000);
+            }
+
+            if(!player.onFoor){
+                camera.target().y(camera.target().y() + 0.1f * GetFrameTime() * 1000);
+            }
+
+            if (IsKeyDown(KEY_A)) {
+                camera.target().x(camera.target().x() + (-velosityX) * GetFrameTime() * 1000);
+            }
+
+
+            if (IsKeyDown(KEY_D)) {
+                camera.target().x(camera.target().x() + velosityX1 * GetFrameTime() * 1000);
+            }
+
+            player.x = (int)camera.target().x();
+            player.y = (int)camera.target().y();
+            camera.zoom((camera.zoom()) + (GetMouseWheelMove() * 0.03f));
+            camera.zoom(Math.max(camera.zoom(), 0.02f));
 
             BeginDrawing();
             ClearBackground(RAYWHITE);
             BeginMode2D(camera);
-                for(Atom atom : atoms){
-                    DrawCircle((int)atom.position.x(), (int)atom.position.y(), atom.radius, atom.color);
-                    // Вектор скорости (зелёный)
-                    DrawLine((int)atom.position.x(), (int)atom.position.y(),
-                            (int)(atom.position.x() + atom.velocity.x() * 10),
-                            (int)(atom.position.y() + atom.velocity.y() * 10), GREEN);
-                    // Вектор силы (красный)
-                    DrawLine((int)atom.position.x(), (int)atom.position.y(),
-                            (int)(atom.position.x() + atom.force.x() * 0.1),
-                            (int)(atom.position.y() + atom.force.y() * 0.1), RED);
-                }
+
+            DrawRectangle(object.x, object.y, object.width, object.height, VIOLET);
+
+            DrawEllipse(player.x, player.y, player.width, player.height, RED);
 
             EndMode2D();
 
             DrawFPS(20, 20);
             DrawText("zoom: " + camera.zoom(), 20, 40, 20, RED);
-            DrawText("objects: " + countObjects, 20, 60, 20, RED);
-
             EndDrawing();
-            float totalForce = 0.0f;
-            float totalVelocity = 0.0f;
-            for (Atom atom : atoms) {
-                totalForce += Vector2Length(atom.force);
-                totalVelocity += Vector2Length(atom.velocity);
-            }
-            System.out.println("Total force: " + totalForce + " | Total velocity: " + totalVelocity);
 
+            //столкновение
+            Collision.collision(player, object);
         }
         CloseWindow();
     }
